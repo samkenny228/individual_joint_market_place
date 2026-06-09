36a374177362fcc4c078e75ae5e664f7eac76d49
samkenny228 <79519038+samkenny228@users.noreply.github.com>
2026-06-09 09:03:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="google-adsense-account" content="ca-pub-6385303503211547">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="x-icon" href="Kehkehaskimage.PNG">
+    <title>KehkehAsk</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="mainstyle.css">
+    <script type="text/javascript" src="jquery-3.7.1.js"></script>
+    
+    
+</head>
+<body>
+    <div id="login_message"></div>
+    <div class="container">
+
+        <div class="div_jumbo">
+        <img src="Kehkehaskimage.PNG" style="height: 130px; width:130px;">
+            <h1 class="reg_heading">KehkehAsk <br> Sell your <br> <small class="product_service"> Products And Services </small></h1>
+        <div class="div_log"> 
+            <a href="product/register.php" class="log_link">Register as a product seller</a> <br>
+            <a href="product/login.php" class="log_link">Login as a product seller</a> <br><br>
+            <a href="service/register.php" class="log_link">Register as a service seller</a> <br>
+             <a href="service/login.php" class="log_link">Login as a service seller</a> <br>
+        </div>
+        <div style="height:20px;"></div>
+        </div>
+    </div>
+
+    
+<div class="container">
+<div class="div_jumbo_alert">
+<h5 class="reg_heading">Things to know about KehkehAsk</h5>
+<ul style="list-style-type:square;">
+  <li>KehkehAsk makes your order and delivery service easy</li>
+  <li>KehkehAsk makes your brand stand out</li>
+  <li>Your brand customers can order and still track there orders</li>
+  <li>Your brand will be notified when a new order is made via email or your brand dashboard</li>
+  <li>Payment method Bank transfer to brand bank account details registered</li>
+  <li>KehkehAsk help your brand create online present</li>
+  <li>Complete video guide on how to use kehkehask website to sell your product: <a href="https://youtube.com/shorts/G70yVT3iZ6w?si=gu5mYxr2d6zUrpKu"> Video</a></li>
+</ul>
+
+</div>
+</div>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6385303503211547"
+     crossorigin="anonymous"></script>
+<!-- fixads -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:190px;height:140px"
+     data-ad-client="ca-pub-6385303503211547"
+     data-ad-slot="5525825935"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+<div style="height: 70px;"></div>
+
+<footer class="reg_footer">
+    Get Your Business To Next Level
+</footer>
+
+<script src="jsfile/login.js"></script>
+
+</body>
+</html>
