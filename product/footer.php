560696265db6af48d29f46e48d967d2d8837d632
samkenny228 <79519038+samkenny228@users.noreply.github.com>
2026-06-10 04:10:26 -0500

    Delete product directory

--- product/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="reg_footer">
-    <a href="contactme_page.php" class="log_link">Contact us</a> // <a href="https://kehkehask.com/fy/service/index.php" class="log_link">Service page </a>
-</footer>
